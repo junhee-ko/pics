5b87ca720e33e42f724125d97b56781b4e022938
junhee-ko <junheee.ko@gmail.com>
2022-05-15 16:10:59 +0900

    Update arrow function in onFormSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,33 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {
+    term: ''
+  };
 
-  onInputChange(event) {
-    console.log(event.target.value)
+  onFormSubmit(event) {
+    event.preventDefault() // prevent refresh when pressing enter
+
+    console.log(this.state.term)
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form
+          className="ui form"
+          onSubmit={
+            (e) => this.onFormSubmit(e)
+          }
+        >
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
-              onChange={this.onInputChange}
+              value={this.state.term}
+              onChange={
+                (e) => this.setState({term: e.target.value})
+              }
             />
           </div>
         </form>
